9af767c0959cc58abb86b5d9ee23e7f6cc776dee
kezdetphia <fehermark88@gmail.com>
2024-04-19 00:12:50 -0700

    prep for deploymentt

--- routes/stripe.js
+++ routes/stripe.js
@@ -60,9 +60,9 @@ router.post("/create-checkout-session", async (req, res) => {
     mode: "payment",
     customer: customer.id,
     success_url:
-      "https://rn-ecom-payment-server-production.up.railway.app//stripe/checkout-success",
+      "https://rn-ecom-payment-server-production.up.railway.app/stripe/checkout-success",
     cancel_url:
-      "https://rn-ecom-payment-server-production.up.railway.app//stripe/cancel",
+      "https://rn-ecom-payment-server-production.up.railway.app/stripe/cancel",
   });
 
   // res.redirect(303, session.url);
